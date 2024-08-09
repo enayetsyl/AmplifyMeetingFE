263389a7d0c8ab1eec1bdc5cece62eb06a0960e3
enayetaitech <mdrehman@365aitech.com>
2024-08-09 15:47:42 +0600

    page 153 done

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -4,10 +4,8 @@ import axios from 'axios';
 import InputField from '@/components/shared/InputField';
 import Image from 'next/image';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import { BiSolidErrorAlt } from "react-icons/bi";
 import Logo from '@/components/shared/Logo';
 import registerImage from '../../../../public/register.jpg';
-import logo from '../../../../public/logo.jpg';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
@@ -68,7 +66,7 @@ const Register = () => {
   return (
     <div>
       {/* Top div for lg */}
-      <div className="hidden justify-center items-start lg:flex bg-white h-10">
+      <div className="hidden lg:justify-center lg:items-start lg:flex bg-white h-10">
         {/* left image div */}
         <div className="flex-1 flex items-center w-full h-full">
           <div className='pl-10 pt-8'><Logo /></div>
@@ -76,17 +74,17 @@ const Register = () => {
         <div className="flex-1 bg-custom-gray-2 h-10"></div>
       </div>
       {/* Top div for mobile */}
-      <div className="lg:hidden bg-white flex justify-center items-center pt-10">
-        <Image src={logo} alt="logo" height={120} width={180} />
+      <div className="lg:hidden bg-white flex justify-center items-center pt-5">
+      <Logo/>
       </div>
 
       {/* Bottom div for large screen*/}
-      <div className='lg:flex justify-center items-center hidden'>
+      <div className='lg:flex lg:justify-center lg:items-center '>
         {/* left div */}
-        <div className='flex-1'>
-          <h2 className="text-2xl font-bold text-center mb-6">Register</h2>
-          <form onSubmit={handleSubmit} className='px-32'>
-            <div className='flex justify-between items-center gap-2'>
+        <div className='flex-1 pb-10 lg:pb-0'>
+          <h2 className="text-2xl font-bold text-center mb-6 pt-5 lg:pt-0">Register</h2>
+          <form onSubmit={handleSubmit} className='lg:px-32 px-8'>
+            <div className='lg:flex lg:justify-between lg:items-center lg:gap-2'>
               <InputField
                 label="First Name"
                 name="firstName"
@@ -164,7 +162,7 @@ const Register = () => {
                 </span>
               </label>
               {errors.terms && <p className="text-red-500 text-sm">{errors.terms}</p>}
-              <p className='pl-5 text-xs pt-2'>
+              <p className='lg:pl-5 text-xs pt-2'>
                 Your personal data will be used to support your experience throughout this website to manage access to your account, and for other purposes described in our <span className='text-custom-light-blue-1 underline'>Privacy Policy</span>.
               </p>
             </div>
@@ -181,7 +179,7 @@ const Register = () => {
         </div>
 
         {/* right div */}
-        <div className='flex-1 bg-custom-gray-2 min-h-screen'>
+        <div className='hidden lg:flex lg:flex-1 lg:bg-custom-gray-2 min-h-screen'>
           <div className="flex-1 flex justify-center items-start">
             <Image
               src={registerImage}
@@ -193,103 +191,7 @@ const Register = () => {
         </div>
       </div>
 
-      {/* Bottom div for small screen */}
-      <div className='flex lg:hidden'>
-        <div className='flex-1'>
-          <h2 className="text-2xl font-bold text-center my-6">Register</h2>
-          <form onSubmit={handleSubmit} className='px-10 md:px-28 pt-5'>
-            <InputField
-              label="First Name"
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleChange}
-              placeholder="Enter your first name"
-              error={errors.firstName}
-            />
-            <InputField
-              label="Last Name"
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleChange}
-              placeholder="Enter your last name"
-              error={errors.lastName}
-            />
-            <InputField
-              label="Email"
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="Enter your email"
-              error={errors.email}
-              emailSuccess={emailAvailable}
-            />
-            <InputField
-              label="Password"
-              type={showPassword ? 'text' : 'password'}
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              placeholder="Enter your password"
-              error={errors.password}
-              icon={
-                <button
-                  type="button"
-                  onClick={() => setShowPassword(!showPassword)}
-                  className="focus:outline-none"
-                >
-                  {showPassword ? <FaEyeSlash /> : <FaEye />}
-                </button>
-              }
-            />
-            <InputField
-              label="Confirm Password"
-              type={showPassword ? 'text' : 'password'}
-              name="confirmPassword"
-              value={formData.confirmPassword}
-              onChange={handleChange}
-              placeholder="Confirm your password"
-              error={errors.confirmPassword}
-              icon={
-                <button
-                  type="button"
-                  onClick={() => setShowPassword(!showPassword)}
-                  className="focus:outline-none"
-                >
-                  {showPassword ? <FaEyeSlash /> : <FaEye />}
-                </button>
-              }
-            />
-            <div className="mb-4">
-              <label className="inline-flex items-center">
-                <input
-                  type="checkbox"
-                  className="form-checkbox peer checked:bg-custom-dark-blue-1 border-black rounded-lg"
-                  name="terms"
-                  checked={formData.terms}
-                  onChange={handleChange}
-                />
-                <span className="ml-2">
-                  I agree to the <a href="#" className="text-custom-light-blue-2 font-bold">Terms & Conditions</a>
-                </span>
-              </label>
-              {errors.terms && <p className="text-red-500 text-sm">{errors.terms}</p>}
-              <p className='pl-5 text-xs pt-2'>
-                Your personal data will be used to support your experience throughout this website to manage access to your account, and for other purposes described in our <span className='text-custom-light-blue-1 underline'>Privacy Policy</span>.
-              </p>
-            </div>
-            <button
-              type="submit"
-              className="w-full bg-custom-orange-1 text-white font-semibold py-2 rounded-lg hover:bg-orange-600"
-            >
-              Create Account
-            </button>
-          </form>
-          <p className="mt-4 text-center">
-            Already have an account? <Link href="/login" className="text-custom-light-blue-1">Login</Link>
-          </p>
-        </div>
-      </div>
+
     </div>
   );
 };
