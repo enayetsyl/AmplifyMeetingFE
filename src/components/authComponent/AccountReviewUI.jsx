c21f7de893c1b4d06a1d5c480e8396ead7030820
enayetaitech <mdrehman@365aitech.com>
2024-08-12 17:39:40 +0600

    Screen 159

--- src/components/authComponent/AccountReviewUI.jsx
+++ src/components/authComponent/AccountReviewUI.jsx
@@ -1,36 +1,37 @@
-import React from 'react'
-import HeadingH1 from '../shared/headingH1'
-import ParagraphBlue2 from '../shared/ParagraphBlue2'
-import Button from '../shared/Button'
-import BackToLogin from '../shared/BackToLogin'
-import { FaEnvelopeOpenText } from 'react-icons/fa'
+import React from "react";
+import HeadingH1 from "../shared/headingH1";
+import ParagraphBlue2 from "../shared/ParagraphBlue2";
+import Button from "../shared/Button";
+import BackToLogin from "../shared/BackToLogin";
+import { FaEnvelopeOpenText } from "react-icons/fa";
+import Link from "next/link";
 
 const AccountReviewUI = () => {
   return (
     <div className="py-20">
-       <div className="max-w-[800px] mx-auto shadow_primary px-10 lg:px-20 bg-white rounded-xl">
-         {/* icon div */}
-         <div className="flex justify-center items-center py-5">
-           ⁡⁢⁣⁢ {/* 𝗧𝗼𝗱𝗼- 𝗻𝗲𝗲𝗱 𝘁𝗼 𝗰𝗵𝗮𝗻𝗴𝗲 it to search icon */}⁡
-           <FaEnvelopeOpenText className="h-20 w-20 " />
-         </div>
-         {/* text div */}
-         <div className="px-3">
-           <HeadingH1 children="YOUR ACCOUNT IS UNDER REVIEW" />
-           <ParagraphBlue2 children="We will contact you via email once your account has been approved." />
-           
-         </div>
-         <div className="pt-10 pb-32">
-           <Button
-             children="Back to Login"
-             variant="primary"
-             className="py-2 rounded-2xl w-full font-bold text-xl"
-           />
-         </div>
-         
-       </div>
-     </div>
-  )
-}
+      <div className="max-w-[800px] mx-auto shadow_primary px-14 lg:px-20 bg-white rounded-xl">
+        {/* icon div */}
+        <div className="flex justify-center items-center py-5">
+          ⁡⁢⁣⁢ {/* 𝗧𝗼𝗱𝗼- 𝗻𝗲𝗲𝗱 𝘁𝗼 𝗰𝗵𝗮𝗻𝗴𝗲 it to search icon */}⁡
+          <FaEnvelopeOpenText className="h-20 w-20 " />
+        </div>
+        {/* text div */}
+        <div className="px-3">
+          <HeadingH1 children="YOUR ACCOUNT IS UNDER REVIEW" />
+          <ParagraphBlue2 children="We will contact you via email once your account has been approved." />
+        </div>
+        <div className="pt-10 pb-32">
+          <Link href={"/login"}>
+            <Button
+              children="Back to Login"
+              variant="primary"
+              className="py-2 rounded-2xl w-full font-bold text-xl cursor-pointer"
+            />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default AccountReviewUI
+export default AccountReviewUI;
