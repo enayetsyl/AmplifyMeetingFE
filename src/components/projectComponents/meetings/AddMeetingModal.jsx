8e8f239c817ccd324923ac6e42429086b3f4ca37
enayetaitech <mdrehman@365aitech.com>
2024-09-02 18:47:58 +0600

    button import corrected

--- src/components/projectComponents/meetings/AddMeetingModal.jsx
+++ src/components/projectComponents/meetings/AddMeetingModal.jsx
@@ -1,4 +1,4 @@
-import Button from "@/components/shared/Button";
+import Button from "@/components/shared/button";
 import Dropdown from "@/components/shared/Dropdown";
 import FormDropdownLabel from "@/components/shared/FormDropdownLabel";
 import InputField from "@/components/shared/InputField";
