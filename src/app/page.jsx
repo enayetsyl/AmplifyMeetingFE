ef31f9e2fa53c3ed860e677a4a477d95d4d8e060
enayetaitech <mdrehman@365aitech.com>
2024-09-02 16:14:53 +0600

    home page edited

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,10 +8,30 @@ import Error401 from "@/components/errorComponent/Error401";
 import Error403 from "@/components/errorComponent/Error403";
 import Error500 from "@/components/errorComponent/Error500";
 import Error404 from "@/components/errorComponent/Error404";
+import Link from "next/link";
+import HeadingBlue25px from "@/components/shared/HeadingBlue25px";
 
 export default function Home() {
   return (
-<Error404/>    
+    <div className="flex flex-col justify-center items-center min-h-screen meeting_bg space-y-5">
+      <HeadingBlue25px>Welcome to Amplify Research</HeadingBlue25px>
+      <Link href="/login">
+      <Button
+      children='Login'
+      variant="primary"
+      className="px-4 py-2 rounded-lg"
+      type="button      "
+      />
+      </Link>
+      <Link href="/register">
+      <Button
+      children='Register'
+      variant="primary"
+      className="px-4 py-2 rounded-lg"
+      type="button      "
+      />
+      </Link>
+    </div>
   
   );
 }
