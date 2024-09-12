3a3d8472fb4ccf667bf14c755ac652401120531f
enayetaitech <mdrehman@365aitech.com>
2024-09-12 11:44:47 +0600

    eslint file changed

--- src/components/authComponent/AccountActivation.jsx
+++ src/components/authComponent/AccountActivation.jsx
@@ -1,23 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import HorizontalBackground from '../shared/HorizontalBackground';
-import AccountActivationUI from './AccountActivationUI';
+import HorizontalBackground from "../shared/HorizontalBackground";
+import AccountActivationUI from "./AccountActivationUI";
 const AccountActivation = () => {
   return (
-   <div> 
-    {/*  For large screen */}
-   <div className='hidden lg:block'>
-<HorizontalBackground>
-<AccountActivationUI/>
-   </HorizontalBackground>
-   </div>
-   {/* // For small screen */}
-   <div className='lg:hidden'>
-
-   <AccountActivationUI/>
-   
-   </div>
-   </div>
+    <div>
+      {/*  For large screen */}
+      <div className="hidden lg:block">
+        <HorizontalBackground>
+          <AccountActivationUI />
+        </HorizontalBackground>
+      </div>
+      {/* // For small screen */}
+      <div className="lg:hidden">
+        <AccountActivationUI />
+      </div>
+    </div>
   );
 };
 
